07a13538de6fe6d896fd9e3255222f2ffba7f596
BigBinary <bigbinary@BigBinarys-MacBook-Pro.local>
2025-02-03 18:15:46 +0530

    added carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % imageUrls.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrevious = () => {
+    const previousIndex =
+      (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+    setCurrentIndex(previousIndex);
+  };
+
+  return (
+    <div className="flex items-center">
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Left}
+        style="text"
+        onClick={handlePrevious}
+      />
+      <img
+        alt={title}
+        className="max-w-56 h-56 max-h-56 w-56"
+        src={imageUrls[currentIndex]}
+      />
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Right}
+        style="text"
+        onClick={handleNext}
+      />
+    </div>
+  );
+};
+
+export default Carousel;
