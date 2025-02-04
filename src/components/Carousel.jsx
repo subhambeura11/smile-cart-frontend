da4daec5eda5975bf1a624ede97b28c029a1aafd
BigBinary <bigbinary@BigBinarys-MacBook-Pro.local>
2025-02-04 10:37:17 +0530

    styles carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,25 +19,27 @@ const Carousel = ({ imageUrls, title }) => {
   };
 
   return (
-    <div className="flex items-center">
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Left}
-        style="text"
-        onClick={handlePrevious}
-      />
-      <img
-        alt={title}
-        className="max-w-56 h-56 max-h-56 w-56"
-        src={imageUrls[currentIndex]}
-      />
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Right}
-        style="text"
-        onClick={handleNext}
-      />
-      <div className="flex space-x-1">
+    <div className="flex flex-col items-center space-y-3">
+      <div className="flex items-center space-x-1">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={handleNext}
+        />
+      </div>
+      <div className="flex  space-x-1">
         {imageUrls.map((_, index) => {
           const defaultClasses =
             "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border";
