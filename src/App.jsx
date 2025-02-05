4f4df8a293562d1e6840d32937bb306f319f1373
Subham Beura <subham.beura@bigbinary.com>
2025-02-05 14:57:50 +0530

    Standardized frontend routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import PageNotFound from "components/commons/PageNotFound";
 import Product from "components/Product";
 import ProductList from "components/ProductList";
 import { Redirect, Route, Switch } from "react-router-dom";
+import routes from "routes";
 
 import "./App.css";
 
 const App = () => (
   <Switch>
-    <Route exact component={ProductList} path="/products" />
-    <Route exact component={Product} path="/products/:slug" />
-    <Redirect exact from="/" to="/products" />
+    <Route exact component={ProductList} path={routes.products.index} />
+    <Route exact component={Product} path={routes.products.show} />
+    <Redirect exact from={routes.root} to={routes.products.index} />
     <Route component={PageNotFound} path="*" />
   </Switch>
 );
